4ae17306331cc0a6d3c8600c03bdb0eca6183f42
Guilherme Nono <guilherme.nono45789@gmail.com>
2022-08-22 03:20:40 -1200

    Add new command
    - Adding new command to set new roles to a profile

---
+++ src/Commands/Config/SetProfile.ts
@@ -0,0 +1,26 @@
+import { Bot } from "../../Client/Client";
+import { Message } from "discord.js";
+import Command from "../../Core/Command";
+import DatabaseCreator from "../../util/DatabaseCreator";
+
+
+export default class SetProfile extends Command {
+
+    constructor(client: Bot) {
+        super(client, {
+            name: 'SetProfile',
+            cooldown: 1000,
+            requiredPermissions: ["SendMessages"],
+            category: "Information",
+            usage: client.settings.prefix?.concat('Ping'),
+            description: "Espera o 'pong' como retorno do bot.",
+            onlyMaster: true
+        });
+    }
+
+    public async run(client: Bot, message: Message, args: string[]): Promise<void> {
+
+        await new DatabaseCreator(message).profileSchema({name:"ADMIN", roles:["595008072951267328"]});
+
+    }
+}
